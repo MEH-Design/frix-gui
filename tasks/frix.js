5d77be2b3daa3aecc654b4e3027e9732af9dc840
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2017-09-04 09:13:13 +0200

    added auto-commit and auto-push

--- tasks/frix.js
+++ tasks/frix.js
@@ -1,11 +1,25 @@
 const frix = require('frix');
 const appRoot = require('app-root-path');
 const gutil = require('gulp-util');
+const shell = require('shelljs');
+const gitconf = require('../gitconf.js');
+
+var pushTimeout;
 
 module.exports = () => {
   frix.api.getOpt().root += 'frix/';
   frix.api.watchReRender((data) => {
     gutil.log(`${data.render.key} rendered`);
+    if(gitconf) {
+      let file = frix.api.getOpt().root + frix.api.keys[data.render.key].content;
+      shell.exec(`git add ${file} && git commit -m "${gitconf.message}"`);
+      gutil.log(`auto commit ${file}`);
+      if(pushTimeout) clearTimeout(pushTimeout);
+      pushTimeout = setTimeout(() => {
+        shell.exec(`git push https://${gitconf.user}:${gitconf.pass}@${gitconf.remote} --all`);
+        gutil.log(`auto push to ${gitconf.remote}`);
+      }, 1000 * 60 * 60); // 1 hour
+    }
   });
   return frix.render().then((requestHandler) => {
     // temporary workaround
