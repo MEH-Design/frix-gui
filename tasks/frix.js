50b1a7f4147a39f4cd2d70445c5d4d41eb361075
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2017-09-26 14:15:17 +0100

    update frix, add error handling for gitconf fix #21

--- tasks/frix.js
+++ tasks/frix.js
@@ -4,6 +4,7 @@ const gutil = require('gulp-util');
 const shell = require('shelljs');
 const optional = require('optional');
 const gitconf = optional(`${appRoot}/gitconf.js`);
+const gitconfProps = ['user', 'pass', 'remote', 'message', 'email', 'name', 'timeout'];
 
 var pushTimeout;
 
@@ -12,14 +13,20 @@ module.exports = () => {
   frix.api.watchReRender((data) => {
     gutil.log(`${data.render.key} rendered`);
     if(gitconf) {
-      let file = frix.api.getOpt().root + 'content/' +  frix.api.keys[data.render.key].content;
-      shell.exec(`git add ${file} && git commit -c "user.name=${gitconf.name}" -c "user.email=${gitconf.email}" -m "${gitconf.message}"`);
-      gutil.log(`auto commit ${file}`);
-      if(pushTimeout) clearTimeout(pushTimeout);
-      pushTimeout = setTimeout(() => {
-        shell.exec(`git push https://${gitconf.user}:${gitconf.pass}@${gitconf.remote} --all`);
-        gutil.log(`auto push to ${gitconf.remote}`);
-      }, gitconf.timeout);
+      if(gitconfProps.every((x) => x in gitconf)) {
+        let file = frix.api.getOpt().root + 'content/' +  frix.api.keys[data.render.key].content;
+        shell.exec(`git add ${file} && git commit -c "user.name=${gitconf.name}" -c "user.email=${gitconf.email}" -m "${gitconf.message}"`);
+        gutil.log(`auto commit ${file}`);
+        if(pushTimeout) clearTimeout(pushTimeout);
+        pushTimeout = setTimeout(() => {
+          shell.exec(`git push https://${gitconf.user}:${gitconf.pass}@${gitconf.remote} --all`);
+          gutil.log(`auto push to ${gitconf.remote}`);
+        }, gitconf.timeout);
+      } else {
+        gutil.log(gutil.colors.yellow('some properties in gitconf.js not found. Valid properties are ' + gitconfProps));
+      }
+    } else {
+      gutil.log(gutil.colors.yellow('gitconf.js not found, no autocommits will be made'));
     }
   });
   return frix.render().then((requestHandler) => {
